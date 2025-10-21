e5f462d7d78faa965a1f703ceb28d2a9e82aef16
User <poltaradnev@gmail.com>
2025-10-21 18:34:41 +0400

    feat: resolve first task and check commit

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity !== 'string' || 
+    !sampleActivity || 
+    isNaN(+sampleActivity) || 
+    +sampleActivity <= 0 || 
+    +sampleActivity > MODERN_ACTIVITY
+  ) {
+    return false;
+  }
+
+  const activity = +sampleActivity;
+
+  const age = Math.log(MODERN_ACTIVITY / activity) * HALF_LIFE_PERIOD / Math.LN2;
+
+  return Math.ceil(age);
 }
 
 module.exports = {
